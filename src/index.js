3c89014c8e1d13390b320afeacfa8dc4170bbb95
Yang Chen <yangchen@thinkmore.info>
2018-09-26 05:15:45 -0400

    add hyperscript support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {div,h} from "react-hyperscript-helpers";
 
 const Index = () => {
-  return <div>Hello React!</div>;
+  return div( "Hello React!" );
 };
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render( h(Index), document.getElementById("index"));
